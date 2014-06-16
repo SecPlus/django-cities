c869ab226a777d0098889b50b07c9db15b13a00b
Tiago Natel de Moura <tiago4orion@gmail.com>
2014-06-15 21:53:54 -0300

    Added Multipolygon field to each model

--- cities/models.py
+++ cities/models.py
@@ -3,15 +3,17 @@ from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from conf import settings
 
+from django.utils.translation import ugettext_lazy as _
+
 __all__ = [
         'Point', 'Country', 'Region', 'Subregion',
-        'City', 'District', 'PostalCode', 'AlternativeName', 
+        'City', 'District', 'PostalCode', 'AlternativeName',
 ]
 
 class Place(models.Model):
-    name = models.CharField(max_length=200, db_index=True, verbose_name="ascii name")
+    name = models.CharField(max_length=200, db_index=True, verbose_name=_("ascii name"))
     slug = models.CharField(max_length=200)
-    alt_names = models.ManyToManyField('AlternativeName')
+    alt_names = models.ManyToManyField('AlternativeName', verbose_name=_(u"Alternative name"))
 
     objects = models.GeoManager()
 
@@ -31,19 +33,23 @@ class Place(models.Model):
     def __unicode__(self):
         return force_unicode(self.name)
 
+
 class Country(Place):
-    code = models.CharField(max_length=2, db_index=True)
-    code3 = models.CharField(max_length=3, db_index=True)
-    population = models.IntegerField()
-    area = models.IntegerField(null=True)
-    currency = models.CharField(max_length=3, null=True)
-    currency_name = models.CharField(max_length=50, null=True)
-    languages = models.CharField(max_length=250, null=True)
-    phone = models.CharField(max_length=20)
-    continent = models.CharField(max_length=2)
-    tld = models.CharField(max_length=5)
-    capital = models.CharField(max_length=100)
-    neighbours = models.ManyToManyField("self")
+    code = models.CharField(max_length=2, db_index=True, verbose_name=_(u"Code"))
+    code3 = models.CharField(max_length=3, verbose_name=_(u"Code3"))
+    population = models.IntegerField(verbose_name=_(u"Population"))
+    area = models.IntegerField(null=True, verbose_name=_(u"Area"))
+    currency = models.CharField(max_length=3, null=True, verbose_name=_(u"Currency"))
+    currency_name = models.CharField(max_length=50, null=True, verbose_name=_(u"Currency name"))
+    languages = models.CharField(max_length=250, null=True, verbose_name=_(u"Languages"))
+    phone = models.CharField(max_length=20, verbose_name=_(u"Phone"))
+    continent = models.CharField(max_length=2, verbose_name=_(u"Continent"))
+    tld = models.CharField(max_length=5, verbose_name=_(u"TLD"))
+    capital = models.CharField(max_length=100, verbose_name=_(u"Capital"))
+    neighbours = models.ManyToManyField("self", verbose_name=_(u"Neighbours"))
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     class Meta:
         ordering = ['name']
@@ -57,9 +63,12 @@ class Country(Place):
         return force_unicode(self.name)
 
 class Region(Place):
-    name_std = models.CharField(max_length=200, db_index=True, verbose_name="standard name")
+    name_std = models.CharField(max_length=200, db_index=True, verbose_name="Standard name")
     code = models.CharField(max_length=200, db_index=True)
-    country = models.ForeignKey(Country)
+    country = models.ForeignKey(Country, verbose_name=_(u"Country"))
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     @property
     def parent(self):
@@ -72,6 +81,9 @@ class Subregion(Place):
     name_std = models.CharField(max_length=200, db_index=True, verbose_name="standard name")
     code = models.CharField(max_length=200, db_index=True)
     region = models.ForeignKey(Region)
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     @property
     def parent(self):
@@ -89,7 +101,10 @@ class City(Place):
     country = models.ForeignKey(Country)
     elevation = models.IntegerField(null=True)
     kind = models.CharField(max_length=10) # http://www.geonames.org/export/codes.html
-    timezone = models.CharField(max_length=40) 
+    timezone = models.CharField(max_length=40)
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     class Meta:
         verbose_name_plural = "cities"
@@ -103,6 +118,9 @@ class District(Place):
     location = models.PointField()
     population = models.IntegerField()
     city = models.ForeignKey(City)
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     @property
     def parent(self):
@@ -114,6 +132,9 @@ class AlternativeName(models.Model):
     is_preferred = models.BooleanField(default=False)
     is_short = models.BooleanField(default=False)
     is_colloquial = models.BooleanField(default=False)
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
+
+    objects = models.GeoManager()
 
     def __unicode__(self):
         return "%s (%s)" % (force_unicode(self.name), force_unicode(self.language))
@@ -128,6 +149,7 @@ class PostalCode(Place):
     region_name = models.CharField(max_length=100, db_index=True)
     subregion_name = models.CharField(max_length=100, db_index=True)
     district_name = models.CharField(max_length=100, db_index=True)
+    boundary = models.MultiPolygonField(verbose_name=_(u"Boundaries"))
 
     objects = models.GeoManager()
 
@@ -138,7 +160,7 @@ class PostalCode(Place):
     @property
     def name_full(self):
         """Get full name including hierarchy"""
-        return u', '.join(reversed(self.names)) 
+        return u', '.join(reversed(self.names))
 
     @property
     def names(self):
